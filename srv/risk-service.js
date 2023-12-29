84763e81d2d62f9f9c1f5f17d11c8d7558a951cb
A1034 SAP_Learning <a1034>
2023-12-29 14:55:23 +0000

    Not finished. Still has some troubles with password.

--- srv/risk-service.js
+++ srv/risk-service.js
@@ -6,6 +6,8 @@ const cds = require("@sap/cds");
 module.exports = cds.service.impl(async function () {
     // Define constants for the Risk and BusinessPartners entities from the risk-service.cds file 
     const { Risks, BusinessPartners } = this.entities;
+    console.log("call Common");
+    console.log("process.env.apikey:"+process.env.apikey);
     /**
      *  Set criticality after a READ operation on /risks
      */
@@ -25,10 +27,12 @@ module.exports = cds.service.impl(async function () {
      *  Each request to the API Business Hub requires the apikey in the header.
      */
     this.on("READ", BusinessPartners, async (req) => {
+        console.log("call BusinessPartners");
+        console.log("process.env.apikey:"+process.env.apikey);
         // The API Sandbox returns alot of business partners with empty names. 
         // We don't want them in our application
         req.query.where("LastName <> '' and FirstName <> '' ");
-        // console.log("process.env.apikey:"+process.env.apikey);
+        // console.log("apikey:"+apikey);
 
         // req.query.SELECT.count = false;
         return await BPsrv.transaction(req).send({
@@ -49,6 +53,8 @@ module.exports = cds.service.impl(async function () {
         the risk entity and the business partner entity are in different systems (SAP BTP and S/4 HANA Cloud), 
         if there is such an expand, remove it 
         */
+        console.log("call Risks");
+        console.log("process.env.apikey:"+process.env.apikey);
         if (!req.query.SELECT.columns) return next(); const expandIndex = req.query.SELECT.columns.findIndex(({ expand, ref }) => expand && ref[0] === "bp"); console.log(req.query.SELECT.columns); if (expandIndex < 0) return next(); req.query.SELECT.columns.splice(expandIndex, 1); if (!req.query.SELECT.columns.find((column) => column.ref.find((ref) => ref == "bp_BusinessPartner"))) { req.query.SELECT.columns.push({ ref: ["bp_BusinessPartner"] }); } /* Instead of carrying out the expand, issue a separate request for each business partner This code could be optimized, instead of having n requests for n business partners, just one bulk request could be created */ try {
             res = await next(); res = Array.isArray(res) ? res : [res]; await Promise.all(res.map(async (risk) => {
                 const bp = await BPsrv.transaction(req).send({
